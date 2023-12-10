eee09b76f324154fbacf5b5c2600c2ebec36a21e
Max Fortun <max@maxf.net>
2023-12-10 02:26:22 -0500

    updated

--- replication/replica.sh
+++ replication/replica.sh
@@ -4,5 +4,10 @@ SWD=$( cd $(dirname $0); pwd )
 
 . $SWD/setenv.sh
 
-conf=$( mongo-main --quiet --eval 'EJSON.stringify(rs.conf())' )
-echo "$conf" | jq -r '.members[] | select(.host == "'"$node:27017"'").host' || mongo-main --quiet --eval 'rs.add( { host: "'$node:27017'", priority: 0 } )'
+conf=$( mongo-main --quiet --eval 'EJSON.stringify(rs.conf())' || true )
+host=$( echo "$conf" | jq -r '.members[] | select(.host == "'"$node:27017"'").host' )
+
+if [ -z $host ]; then
+	mongo-main --quiet --eval 'rs.add( { host: "'"$node:27017"'", priority: 0 } )'
+fi
+
