b900141eaa713511102ec460cf165647742a20dd
Max Fortun <max@maxf.net>
2023-12-09 23:06:22 -0500

    updated

--- replication/replica.sh
+++ replication/replica.sh
@@ -6,4 +6,5 @@ SWD=$( cd $(dirname $0); pwd )
 
 echo replica
 
-mongo-admin --quiet --eval 'rs.add( { host: "'$DPSRV_REGION-$DPSRV_NODE.$DPSRV_DOMAIN:27017'" } )'
+conf=$( mongo-main --quiet --eval 'EJSON.stringify(rs.conf())' )
+echo "$conf" | jq '.members[] | select(.host == "'$DPSRV_REGION-$DPSRV_NODE.$DPSRV_DOMAIN:27017'").host' || mongo-admin --quiet --eval 'rs.add( { host: "'$DPSRV_REGION-$DPSRV_NODE.$DPSRV_DOMAIN:27017'" } )'
