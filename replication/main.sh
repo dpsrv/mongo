0aadfc8d8969b7950531820d6ecc2a63bb027a86
Maxim Fortun <max@maxf.net>
2023-12-10 02:06:09 -0500

    updated

--- replication/main.sh
+++ replication/main.sh
@@ -5,15 +5,17 @@ SWD=$( cd $(dirname $0); pwd )
 . $SWD/setenv.sh
 
 conf=$( mongo-local --quiet --eval 'EJSON.stringify(rs.conf())' || true )
-echo "rs.conf: $conf"
-
 host=$( echo "$conf" | jq -r '.members[] | select(.host == "'$node:27017'").host' )
 
 if [ -z $host ]; then
 	mongo-local --quiet --eval 'rs.initiate({
-		_id: "dpsrv", members: [ { _id: 0, host: "'$node:27017'" } ]
+		_id: "dpsrv", members: [ { _id: 0, host: "'"$node:27017"'", priority: 1 } ]
 	})' 
 	exit 1
 fi
 
-echo "main node $host"
+conf=$( echo "$conf" | jq '(.members[] | select(.host != "$node:27017")).priority |= 0' )
+conf=$( echo "$conf" | jq '(.members[] | select(.host == "$node:27017")).priority |= 1' )
+
+mongo-local --quiet --eval 'rs.reconfig('"$conf"')'
+
--- replication/replica.sh
+++ replication/replica.sh
@@ -4,7 +4,5 @@ SWD=$( cd $(dirname $0); pwd )
 
 . $SWD/setenv.sh
 
-echo replica
-
 conf=$( mongo-main --quiet --eval 'EJSON.stringify(rs.conf())' )
-echo "$conf" | jq -r '.members[] | select(.host == "'$node:27017'").host' || mongo-admin --quiet --eval 'rs.add( { host: "'$node:27017'" } )'
+echo "$conf" | jq -r '.members[] | select(.host == "'"$node:27017"'").host' || mongo-main --quiet --eval 'rs.add( { host: "'$node:27017'", priority: 0 } )'
