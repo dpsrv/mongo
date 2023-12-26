271879d74e1db4889c55f6a9d3cb172f6de6834e
Max <max@maxf.net>
2023-12-25 20:42:40 -0500

    updated

--- replication/replication.sh
+++ replication/replication.sh
@@ -5,14 +5,22 @@ SWD=$( cd $(dirname $0); pwd )
 . $SWD/setenv.sh
 
 if [ $DPSRV_MONGO_NODE != true ]; then
-	$SWD/offline.sh 2>&1 | xargs echo "$(date +%Y-%m-%d\ %H:%M:%S)"
+	touch /tmp/replication.offline
+else
+	rm -f /tmp/replication.offline || true
 fi
 
 while true; do
-	host $main | grep -v NXDOMAIN | sort > /tmp/replication.host 2>/dev/null
+	if [ -f /tmp/replication.offline ]; then
+		$SWD/offline.sh 2>&1 | xargs echo "$(date +%Y-%m-%d\ %H:%M:%S)"
+		sleep 60
+		continue
+	fi
 
+	host $main | grep -v NXDOMAIN | sort > /tmp/replication.host 2>/dev/null
 	if diff -q /tmp/replication.host /tmp/replication.last-host 2>/dev/null; then
 		sleep 60
+		continue
 	fi
 
 	unset complete
