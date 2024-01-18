3f9ba0338f829b49caf0008975d0f29f1079627c
Max <max@maxf.net>
2024-01-18 01:04:20 -0500

    updated

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,5 +5,6 @@
 /opt/replication/replication.sh &
 
 chown -R mongodb:mongodb /etc/mongo/*
+chmod og-rwx /etc/mongo/dpsrv.key
 
 exec /usr/local/bin/docker-entrypoint.sh mongod --config /etc/mongo/mongod.conf
