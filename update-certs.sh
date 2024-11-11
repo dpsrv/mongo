7045e3e794400b6e188c26f3e8cb1844a5cecdf6
Maxim Fortun <max@maxf.net>
2024-11-11 23:12:53 +0000

    updated

--- update-certs.sh
+++ update-certs.sh
@@ -1,8 +1,19 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
-cat /etc/letsencrypt/live/domain/privkey.pem /etc/letsencrypt/live/domain/cert.pem > /etc/mongo/cert.pem
+. /opt/replication/setenv.sh
+
+files="/etc/letsencrypt/live/domain/privkey.pem /etc/letsencrypt/live/domain/cert.pem"
+
+cat $files > /etc/mongo/cert.pem
+last=$( ls -1t $files | head -1 | xargs date +%s -r )
 
 while true; do
-	echo "Checking certs"
-	sleep 120
+	latest=$( ls -1t $files | head -1 | xargs date +%s -r )
+	sleep 60
+
+	if [ $latest -gt $last ]; then
+		last=$latest
+		mongo-local --eval 'db.rotateCertificates();'
+	fi
+
 done &
