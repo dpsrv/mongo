ef8593625e1db9a2c5fcd9224f3aded4e0d22018
Max <max@maxf.net>
2025-04-10 19:06:48 -0400

    updated

--- mongo-admin.sh
+++ mongo-admin.sh
@@ -14,8 +14,13 @@ fi
 . $DPSRV_HOME/rc/bin/dpsrv.sh
 
 MONGO_HOST=${MONGO_HOST:-mongo-main.dpsrv.me}
-admin_uri="mongodb://$MONGO_INITDB_ROOT_USERNAME:$MONGO_INITDB_ROOT_PASSWORD@$MONGO_HOST:27017/admin?tls=true&tlsInsecure=true&tlsCertificateKeyFile=/etc/mongo/cert.pem"
+if [ -x /usr/bin/mongosh ]; then
+	admin_uri="mongodb://$MONGO_INITDB_ROOT_USERNAME:$MONGO_INITDB_ROOT_PASSWORD@$MONGO_HOST:27017/admin?tls=true&tlsInsecure=true&tlsCertificateKeyFile=$DPSRV_HOME/rc/secrets/mongo/conf/cert.pem"
+	mongosh "$admin_uri"
+else
+	admin_uri="mongodb://$MONGO_INITDB_ROOT_USERNAME:$MONGO_INITDB_ROOT_PASSWORD@$MONGO_HOST:27017/admin?tls=true&tlsInsecure=true&tlsCertificateKeyFile=/etc/mongo/cert.pem"
 
-container=$(dpsrv-list | grep ' dpsrv-mongo-' | awk '{ print $3 }')
-docker exec -it $container mongosh "$admin_uri" 
+	container=$(dpsrv-list | grep ' dpsrv-mongo-' | awk '{ print $3 }')
+	docker exec -it $container mongosh "$admin_uri"
+fi
 
