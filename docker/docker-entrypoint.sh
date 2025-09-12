caa179cf6266a463487084fb9371eb1a60e641a4
Max <max@maxf.net>
2025-09-12 02:27:12 +0000

    updated

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -10,7 +10,7 @@ chmod -R u=r,og= /etc/mongo/*
 
 /opt/update-certs.sh 
 
-#/opt/replication/replication.sh &
+/opt/replication/replication.sh &
 
 . /opt/replication/setenv.sh
 exec /usr/local/bin/docker-entrypoint.sh mongod --config /etc/mongo/mongod.conf --replSet $DPSRV_MONGO_CLUSTER
