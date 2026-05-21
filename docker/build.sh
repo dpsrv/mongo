a4ac4508a673750b926975002fb84c99ed97b3b0
Max <max@maxf.net>
2026-05-21 05:02:38 +0200

    updated

--- docker/build.sh
+++ docker/build.sh
@@ -11,4 +11,9 @@ docker compose build
 
 docker push $image
 
+while read -u 3 node_id; do
+	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -n root@$node_id.$DPSRV_DOMAIN "k3s ctr images ls|awk '{ print \$1 }'|grep '$image\$' | xargs -L1 k3s ctr images rm " &
+done 3< <(kubectl get nodes -o json|jq -r '.items[].metadata.name')
+wait
+
 kubectl -n dpsrv rollout restart sts mongodb
